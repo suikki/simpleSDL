3ec8e7451dbf142fc16fd4bd90f5f3bd860db50b
Olli Kallioinen <olli.kallioinen@iki.fi>
2017-06-05 14:43:37 +0300

    Emscripten build support added

--- source/main.c
+++ source/main.c
@@ -7,14 +7,61 @@
 #include <SDL_opengl.h>
 #endif
 
+#if defined(EMSCRIPTEN)
+#include <emscripten.h>
+#endif
+
 #include <stdio.h>
 
 
 static int done = 0;
 static SDL_Window* window = NULL;
-static SDL_GLContext glContext;
+static SDL_GLContext glContext = NULL;
 
 static float blinking = 0.0f;
+
+int init() {
+    int width = 640;
+    int height = 480;
+    Uint32 flags = SDL_WINDOW_OPENGL;
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
+        printf("SDL init failed: %s\n", SDL_GetError());
+        SDL_ClearError();
+        return 0;
+    }
+
+    window = SDL_CreateWindow("SimpleSDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, flags);
+    if (!window) {
+        printf("Creating window failed: %s\n", SDL_GetError());
+        SDL_ClearError();
+        return 0;
+    }
+
+    glContext = SDL_GL_CreateContext(window);
+    if (!glContext) {
+        printf("Creating GL context failed: '%s'.\n", SDL_GetError());
+        SDL_ClearError();
+        return 0;
+    }
+
+    printf("Initialized.\n");
+    return 1;
+}
+
+void release() {
+    if (glContext) {
+        SDL_GL_DeleteContext(glContext);
+        glContext = 0;
+    }
+
+    if (window) {
+        SDL_DestroyWindow(window);
+        window = 0;
+    }
+
+    SDL_Quit();
+}
 
 void renderFrame() {
     SDL_GL_MakeCurrent(window, glContext);
@@ -30,52 +77,35 @@ void renderFrame() {
     SDL_GL_SwapWindow(window);
 }
 
-int main(int argc, char* argv[]) {
-    int width = 640;
-    int height = 480;
-    Uint32 flags = SDL_WINDOW_OPENGL;
-
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
-        printf("SDL init failed: %s\n", SDL_GetError());
-        SDL_ClearError();
-        return 1;
-    }
-
-    window = SDL_CreateWindow("SimpleSDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, flags);
-    if (!window) {
-        printf("Creating window failed: %s\n", SDL_GetError());
-        SDL_ClearError();
-        SDL_Quit();
-        return 1;
-    }
-
-    glContext = SDL_GL_CreateContext(window);
-    if (!glContext) {
-        printf("Creating GL context failed: '%s'.\n", SDL_GetError());
-        SDL_ClearError();
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    printf("Initialized.\n");
-
-    while (!done) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                done = 1;
-            }
+void mainLoop() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            done = 1;
         }
-
-        renderFrame();
-        SDL_Delay(5);
     }
 
-    SDL_GL_DeleteContext(glContext);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    renderFrame();
+    SDL_Delay(5);
+}
 
+int main(int argc, char* argv[]) {
+    if (!init()) {
+        printf("Initialization failed.\n");
+        release();
+        return 1;
+    }
+
+#if defined(EMSCRIPTEN)
+    emscripten_set_main_loop(mainLoop, 0, 0);
+    return 0;
+#else
+    while (!done) {
+        mainLoop();
+    }
+
+    release();
     printf("All done.\n");
     return 0;
+#endif
 }
